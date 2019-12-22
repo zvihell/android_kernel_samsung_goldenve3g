fb40c408f8cadc94346998561c73698b0ae5e2de
zvihell <arsangeriev77@gmail.com>
2019-12-22 17:38:01 +0200

    init

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jan 13 17:18:42 KST 2016"
+#define LINUX_COMPILE_BY "kihyuk"
+#define LINUX_COMPILE_HOST "kihyuk-Samsung-DeskTop-System"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
